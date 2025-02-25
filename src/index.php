24c16f726fa3676ce7622e0779a1c3effb82db19
Jared Mitchell <jared.mitchell@studiesweekly.com>
2025-02-24 18:16:22 -0700

    add github workflow

--- src/index.php
+++ src/index.php
@@ -1,4 +1,10 @@
 <?php
-   $hostname = getenv('HOSTNAME');
-   echo("<h4>container: $hostname</h4>");
+  $data = [
+   'container' => getenv('HOSTNAME'),
+   'timezone' => getenv('TZ'),
+   'time' => date('Y-m-d H:i:s')
+  ];
+  
+  echo json_encode($data);
+   
 ?>
