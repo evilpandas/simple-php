fcba25d1ec21a0c9a638686609cf0d90dde6b04f
Jared Mitchell <jared.mitchell@studiesweekly.com>
2025-02-26 13:03:33 -0700

    scale deployment to 25 and change color to red

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
    'container' => getenv('HOSTNAME'),
    'timezone' => getenv('TZ'),
    'time' => date('Y-m-d H:i:s'),
-   'color' => 'green'
+   'color' => 'red'
   ];
 
   echo json_encode($data);
